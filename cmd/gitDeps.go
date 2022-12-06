2aaf55a06a503a23cb3cdce02d33d75e8fd4ee65
kreempuff <10726831+kreempuff@users.noreply.github.com>
2022-12-06 09:20:16 -0500

    wip: start the download process
    I should start thinking about how to map this to a http_file repo rule in
    bazel.

--- cmd/gitDeps.go
+++ cmd/gitDeps.go
@@ -76,6 +76,20 @@ var gitDepsCmd = &cobra.Command{
 		}
 
 		logrus.Infof("url: %s/%s/%s, compressed size: %d", manifest.BaseUrl, manifest.Packs[0].RemotePath, manifest.Packs[0].Hash, manifest.Packs[0].CompressedSize)
+
+		filename := ".tgitconfig"
+		p := gitDeps.GetPackfromFileName(filename, *manifest)
+		if p == nil {
+			logrus.Errorf("pack not found for %s", filename)
+		} else {
+			logrus.Infof("pack for %s is %s", filename, p.RemotePath)
+		}
+
+		err = gitDeps.DownloadPack(os.Stdout, p, *manifest)
+		if err != nil {
+			logrus.Error("failed to download pack", err)
+			logrus.Debugf("pack: %s failed to download", p.Hash)
+		}
 	},
 }
 
